e5d8bbd878a175767f93101f05c7151d57dd12f1
Andrew Bernier <a6bernier@gmail.com>
2018-06-25 00:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Andrew"
+var name = "Joe"
 var height = "74"
 
 console.log("Name:")
-console.log(Andrew)
+console.log(Name)
 
 console.log("Height:")
 console.log(height)
